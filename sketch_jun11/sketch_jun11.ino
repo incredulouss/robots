446f3fddccba46e664645d9b4dac039bcce174a8
incredulouss <31053231+incredulouss@users.noreply.github.com>
2018-06-18 13:35:43 +0530

    Add files via upload

---
+++ sketch_jun11/sketch_jun11.ino
@@ -0,0 +1,26 @@
+
+ #include "pins.h"
+ #include "motors.h"
+
+  motors Motors;     //object of the motors clss
+
+  void setup() {
+
+  Serial.begin(9600);
+  IRinit();
+ calibrateSensors();
+ delay(6000);
+  //printReadLine();
+  }
+  
+  void loop() {
+ 
+maze();
+ //printReadLine();
+//printReadRaw();
+  }
+
+
+ 
+  
+  
